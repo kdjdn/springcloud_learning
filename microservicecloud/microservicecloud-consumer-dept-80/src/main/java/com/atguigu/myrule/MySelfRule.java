d94b9563178ad8598460084a763a13aaf5ce934d
kdjdn <42147037@qq.com>
2019-01-02 15:03:49 +0800

    springcloud基础学习代码

---
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/myrule/MySelfRule.java
@@ -0,0 +1,21 @@
+package com.atguigu.myrule;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+
+//这个类不能被@ComponentScan扫描到，不能跟DeptConsumer80_App在一个包，DeptConsumer80_App上面有@ComponentScan
+@Configuration
+public class MySelfRule
+{
+	@Bean
+	public IRule myRule()
+	{
+		//return new RandomRule();// Ribbon默认是轮询，我自定义为随机
+		//return new RoundRobinRule();// Ribbon默认是轮询，我自定义为随机
+		
+		return new RandomRule_ZY();// 我自定义为每台机器5次
+	}
+}
